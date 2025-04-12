612830b7fa3c7801a29c6c7095ccc0994d70fda0
Kayky Rodrigues da Silva <kaykyr.dev@gmail.com>
2025-04-12 03:48:31 -0300

    feat: now we have, Coin, Niobio Ingot and Niobio Ore

---
+++ src/main/java/com/fskayky/lioncoin/LionCoin.java
@@ -0,0 +1,72 @@
+package com.fskayky.lioncoin;
+
+import com.fskayky.lioncoin.block.ModBlocks;
+import com.fskayky.lioncoin.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(LionCoin.MOD_ID)
+public class LionCoin {
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "lioncoin";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+    public LionCoin(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.COIN);
+            event.accept(ModItems.NIOBIO);
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.NIOBIO_BLOCK);
+        }
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
---
+++ src/main/java/com/fskayky/lioncoin/item/ModItems.java
@@ -0,0 +1,21 @@
+package com.fskayky.lioncoin.item;
+
+import com.fskayky.lioncoin.LionCoin;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(LionCoin.MOD_ID);
+
+    public static final DeferredItem<Item> COIN = ITEMS.register("lioncoin", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> NIOBIO = ITEMS.register("niobio", () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
